fb66ff9b3578f4740db4c3a4c8401139af7027ea
Bixcoitoo <62630402+Bixcoitoo@users.noreply.github.com>
2022-04-20 11:59:11 -0300

    Update wlc.py

--- wlc.py
+++ wlc.py
@@ -2,9 +2,9 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\033[1;31m","\033[1;32m"]
-m = random.choice(color)+"Bem vindo THBD \nAtualizando pacote de dados...\n\n\n\n\n\n\n\n\n\n\nConcluido mãos a obra!!\n"
+m = random.choice(color)+"Bem vindo THBD \nAtualizando pacote de dados...\n\n\n\n\n\n\nConcluido mãos a obra!!\n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
-    time.sleep(0.25)
+    time.sleep(0.20)
 print("")
